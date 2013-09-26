aa2efbbec95470aa34f91b90f8f62ff8abe46a19
Sergey Protko <fesors@gmail.com>
2013-09-27 00:59:46 +0300

    Added utils package: added generator for reading files by chunks

---
+++ server/utils.py
@@ -0,0 +1,10 @@
+
+def read_file_in_chunks(file_object, chunk_size=1024):
+    """Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 1k."""
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
