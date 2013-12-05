18a91bf692065c914ba417ec54ff0cabe822e190
Sergey Protko <fesors@gmail.com>
2013-12-06 01:59:31 +0300

    Fixed typo in options

--- common/args.py
+++ common/args.py
@@ -7,10 +7,11 @@ def create_argument_parser(description):
     """
     parser = argparse.ArgumentParser(description=description)
 
+    parser.version = '1.0'
     parser.add_argument('port', type=int, help='Port to listen')
     parser.add_argument('host', nargs='?', default='localhost', help='Hostname to listen (default: localhost)')
     parser.add_argument('--verbose', '-v', action='store_true', help='Display debug information')
     parser.add_argument('--echo', '-e', action='store_true', help='Run echo server')
-    parser.add_argument('--utp', '-u', action='store_true', help='Use UTP as transport')
+    parser.add_argument('--udp', '-u', action='store_true', help='Use UDP as transport')
 
     return parser
