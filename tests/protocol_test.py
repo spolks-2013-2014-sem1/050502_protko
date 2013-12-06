5084952b795c2bfdbf48af89686a6c336f39d3c2
Sergey Protko <fesors@gmail.com>
2013-12-07 02:12:08 +0300

    Added test for parsing invalid headers key-value pairs

--- tests/protocol_test.py
+++ tests/protocol_test.py
@@ -8,9 +8,17 @@ class ProtocolTest(unittest.TestCase):
         raw_data = 'Method: LIST\nPath: /'
         data = protocol.read_headers(raw_data)
         self.assertDictEqual({
-            'method': protocol.METHOD_LIST,
+            'method': 'list',
             'path': '/'
         }, data, 'Should parse LIST requests with path')
+
+    def test_read_invalid_headers(self):
+        raw_data = 'Method: LIST\nPath\nTest : value'
+        data = protocol.read_headers(raw_data)
+        self.assertDictEqual({
+            'method': 'list',
+            'test': 'value'
+        }, data, 'Should skip all invalid pairs')
 
 
 def main():
